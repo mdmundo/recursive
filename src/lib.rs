c5b86c03ecc77f68874a94c2947844cd160ccc06
Edmundo Paulino <manyymoore@gmail.com>
2021-12-08 17:14:38 -0300

    refactor: 🎨 Publish fns

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,14 @@ mod fact_tests;
 #[cfg(test)]
 mod fib_tests;
 
-fn fib(n: usize) -> usize {
+pub fn fib(n: usize) -> usize {
     match n {
         0 | 1 => n,
         _ => fib(n - 1) + fib(n - 2),
     }
 }
 
-fn fact(n: usize) -> usize {
+pub fn fact(n: usize) -> usize {
     match n {
         0 => 1,
         _ => n * fact(n - 1),
