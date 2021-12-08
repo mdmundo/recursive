113212c18eca5fa57bf10cfc50af7fbb48544a40
Edmundo Paulino <manyymoore@gmail.com>
2021-12-08 12:12:49 -0300

    feat: ✨ Add factorial recursive fn and move rsrcs

---
+++ src/fact_tests.rs
@@ -0,0 +1,43 @@
+use super::*;
+
+#[test]
+fn fact_0() {
+    let result = fact(0);
+    assert_eq!(result, 1);
+}
+
+#[test]
+fn fact_1() {
+    let result = fact(1);
+    assert_eq!(result, 1);
+}
+
+#[test]
+fn fact_2() {
+    let result = fact(2);
+    assert_eq!(result, 2);
+}
+
+#[test]
+fn fact_3() {
+    let result = fact(3);
+    assert_eq!(result, 6);
+}
+
+#[test]
+fn fact_4() {
+    let result = fact(4);
+    assert_eq!(result, 24);
+}
+
+#[test]
+fn fact_5() {
+    let result = fact(5);
+    assert_eq!(result, 120);
+}
+
+#[test]
+fn fact_6() {
+    let result = fact(6);
+    assert_eq!(result, 720);
+}
---
+++ src/fib_tests.rs
@@ -0,0 +1,43 @@
+use super::*;
+
+#[test]
+fn fib_0() {
+    let result = fib(0);
+    assert_eq!(result, 0);
+}
+
+#[test]
+fn fib_1() {
+    let result = fib(1);
+    assert_eq!(result, 1);
+}
+
+#[test]
+fn fib_2() {
+    let result = fib(2);
+    assert_eq!(result, 1);
+}
+
+#[test]
+fn fib_3() {
+    let result = fib(3);
+    assert_eq!(result, 2);
+}
+
+#[test]
+fn fib_4() {
+    let result = fib(4);
+    assert_eq!(result, 3);
+}
+
+#[test]
+fn fib_5() {
+    let result = fib(5);
+    assert_eq!(result, 5);
+}
+
+#[test]
+fn fib_6() {
+    let result = fib(6);
+    assert_eq!(result, 8);
+}
