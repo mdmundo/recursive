a9809159887f86cc99f05e8a2ac08bfea6cb9ca1
Edmundo Paulino <manyymoore@gmail.com>
2021-12-08 12:01:43 -0300

    test: ✅ Add tests for Fibonacci recursive fn

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,48 @@ fn fib(n: usize) -> usize {
 }
 
 #[cfg(test)]
-mod tests {
+mod fib_tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn fib_0() {
+        let result = fib(0);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn fib_1() {
+        let result = fib(1);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn fib_2() {
+        let result = fib(2);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn fib_3() {
+        let result = fib(3);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn fib_4() {
+        let result = fib(4);
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn fib_5() {
+        let result = fib(5);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn fib_6() {
+        let result = fib(6);
+        assert_eq!(result, 8);
     }
 }
